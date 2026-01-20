57b60cd763b72f54adf2b2a7b7d42f7e83394a8e
compass-dexter <145257008+compass-dexter@users.noreply.github.com>
2026-01-20 16:40:59 +0100

    fix: get users group name inside script
    The connection from a a member server to the DC failed when using PowerShell
    AD Cmdlets directly in ansible.windows.win_shell

--- files/esc9.ps1
+++ files/esc9.ps1
@@ -4,8 +4,10 @@ param(
 )
 
 # Get DistinguishedNames
-$domainUsersDN = (Get-ADGroup -Identity "$((Get-ADDomain).DomainSID)-513").DistinguishedName
+$domainUsers = Get-ADGroup -Identity "$((Get-ADDomain).DomainSID)-513"
+$domainUsersDN = $domainUsers.DistinguishedName
+$domainUsersName = $domainUsers.Name
 $esc9userDN = (Get-ADUser -Identity $esc9user).DistinguishedName
 
 #Add GenericAll rights over the esc9user to the Domain Users group
-dsacls "$esc9userDN" /G "$domainUsersDN"":GA"
+dsacls "$esc9userDN" /G "$domainUsersDN"":GA" | Select-String -Pattern $domainUsersName
